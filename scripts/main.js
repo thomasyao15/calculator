acfa88cddc1ce0f798515b693c5bd5218a00f8d2
Thomas <thomasyao15@gmail.com>
2021-07-29 22:20:19 +1000

    Added operator button event listener Update current operator enum

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,11 @@ numberButtons.forEach(numberButton => {
     numberButton.addEventListener("click", handleNumberClick);
 });
 
+const operatorButtons = document.querySelectorAll(".operator");
+operatorButtons.forEach(operatorButton => {
+    operatorButton.addEventListener("click", handleOperatorClick)
+})
+
 function handleNumberClick(e) {
     const mainBuffer = document.getElementById('main-buffer');
     currentBufferContent = mainBuffer.textContent;
@@ -11,6 +16,7 @@ function handleNumberClick(e) {
 
     const amountOfCommas = currentBufferContent.split('.').length - 1;
     if (clickedNumber == '.' && amountOfCommas > 0) {
+        console.log("Already 1 comma in number");
         return;
     }
 
@@ -22,3 +28,25 @@ function handleNumberClick(e) {
     mainBuffer.textContent = currentBufferContent + clickedNumber;
 }
 
+function handleOperatorClick(e) {
+    const clickedOperator = e.target.id.toUpperCase();
+
+    if (currentOperator != operators.NONE) {
+        console.log("Current operator not none");
+        return;
+    }
+    else {
+        console.log("Adding clicked operator to currentOp");
+        currentOperator = operators[clickedOperator];
+    }
+}
+
+const operators = {
+    PLUS: "plus",
+    MINUS: "minus",
+    MULTIPLY: "multiply",
+    DIVIDE: "divide",
+    NONE: "none",
+}
+
+let currentOperator = operators.NONE;
