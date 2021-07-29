3571dad663d17ea8fbab4f77773efd9ea6a1326b
Thomas <thomasyao15@gmail.com>
2021-07-29 22:07:32 +1000

    Added decimal button functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,24 @@
 
 const numberButtons = document.querySelectorAll(".number");
 numberButtons.forEach(numberButton => {
-    numberButton.addEventListener("click", handleNumberClick)
-})
+    numberButton.addEventListener("click", handleNumberClick);
+});
 
 function handleNumberClick(e) {
     const mainBuffer = document.getElementById('main-buffer');
-    mainBuffer.textContent += e.target.textContent;
+    currentBufferContent = mainBuffer.textContent;
+    clickedNumber = e.target.textContent;
+
+    const amountOfCommas = currentBufferContent.split('.').length - 1;
+    if (clickedNumber == '.' && amountOfCommas > 0) {
+        return;
+    }
+
+    // Chop off leading 0
+    if (currentBufferContent.charAt(0) == '0') {
+        currentBufferContent = currentBufferContent.substring(1);
+    }
+
+    mainBuffer.textContent = currentBufferContent + clickedNumber;
 }
 
